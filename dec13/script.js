9049c37d15a0ad0d4405fb80cd582eb4c601b7af
Gopi P <pgopirmd@gmail.com>
2022-12-13 10:32:26 +0530

    Update script.js

--- dec13/script.js
+++ dec13/script.js
@@ -6,6 +6,10 @@ inputUserName.setAttribute("placeholder", "Enter your email");
 inputUserName.setAttribute("type", "email");
 
 const div = document.createElement("div");
-div.append(paragraph, inputUserName);
+// div.append(paragraph, inputUserName);
+// div.append("this is div");
+const a = div.appendChild(paragraph);
+
+console.log(a);
 
 document.body.append(div);
