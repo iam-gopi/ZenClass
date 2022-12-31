1b8a963fa7be0d3c54a6b0040bd664ee56e881da
Gopi P <pgopirmd@gmail.com>
2022-12-31 11:56:33 +0530

    Update script.js

--- dec31/script.js
+++ dec31/script.js
@@ -15,8 +15,12 @@ const populateGrid = (data) => {
     x.avatar
   }" class="img-fluid rounded-circle shadow" style="width:2rem"></td>
   <td>${new Date(x.createdAt).toLocaleString()}</td>
-  <td><button id="delete" class="btn btn-danger btn-sm">Delete</button></td>
-  <td><button id="edit" class="btn btn-success btn-sm">Edit</button></td>
+  <td><button id="delete" class="btn btn-danger btn-sm" onclick="deleteHandler(${
+    x.id
+  }, '${x.name}')">Delete</button></td>
+  <td><button id="edit" class="btn btn-success btn-sm" onclick='editHandler(${
+    x.id
+  })'>Edit</button></td>
   </tr>`;
   });
   let tbody = document.getElementById("tableBodyContent");
@@ -45,6 +49,36 @@ const clickHandler = async function () {
     getAllDetails();
   } catch (error) {
     console.error(error);
+    throw error;
+  }
+};
+
+const deleteHandler = async (id, name) => {
+  if (confirm(`Are you sure to delete ${name} record`)) {
+    try {
+      await fetch(`${baseURL}/api/${id}`, { method: "DELETE" });
+      getAllDetails();
+    } catch (error) {
+      throw new Error(`this error happened ${error}`);
+    }
+  }
+};
+
+const editHandler = async (id) => {
+  const name = prompt("Enter your new name");
+  if (name === "" || name === null) {
+    alert("name is mandatory");
+    return;
+  }
+
+  try {
+    await fetch(`${baseURL}/api/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    });
+    getAllDetails();
+  } catch (error) {
     throw error;
   }
 };
