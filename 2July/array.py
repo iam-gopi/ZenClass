39093e770f3c5f1cf4dd6f571a847e25fba348e7
Gopi P <pgopirmd@gmail.com>
2023-07-01 21:24:15 -0700

    Create array.py

---
+++ 2July/array.py
@@ -0,0 +1,19 @@
+array 
+    -> fixed 
+    -> in memory itstores all array data in sequential manner
+    -> accessing data is quit easy/faster
+    -> array same type element
+    -> access elements using index 
+
+
+import array as ar 
+
+
+arr = ar.array('i',[1,2,3,4,54])
+#arr.append(55)
+#arr.extend([33,44, 'test'])
+
+#del arr[3]
+
+print(arr)
+
