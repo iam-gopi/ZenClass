04463b0dbad918839a9609e1bab29985ec4b9337
Gopi P <pgopirmd@gmail.com>
2023-07-01 20:57:16 -0700

    Create Overload.py

---
+++ 2July/Overload.py
@@ -0,0 +1,11 @@
+
+class Learning:
+    def learning_js(self):
+        print("js")
+    
+    def learning_js(self, a):
+        print("js overload")
+        
+        
+l = Learning()
+l.learning_js()
