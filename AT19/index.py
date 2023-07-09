3407a6decb73c62eb4b2838cb69523cd03bcb1f7
Gopi P <pgopirmd@gmail.com>
2023-07-08 22:32:05 -0700

    Update index.py

--- AT19/index.py
+++ AT19/index.py
@@ -1,1 +1,56 @@
+download python => https://www.python.org/downloads/
+
+# * dynamic language
+# * high level programming language
+# * free & open source
+# * beginner friendly
+# * have oops/structure oriented programming
+
+# dynamic language?
+# dont have to use datatype while declaring a variable
+# DT assign based on the type of value of variable
+
+# variable?
+# a location in memory which going to hold the value
+# ex: a = 178
+
+# Datatype?
+# describe type of data assign to a variable
+# types:
+#     pre defined -> int, str (collection of character), char(single alphabet), boolean, long, double, float and decimal
+#     user defined -> class
+
+# input
+
+# inp = input("Enter your name")
+# print(inp)
+
+# function => set of instructions that can be reused.
+# purpose => reduce duplication, increase code readability
+
+# type casting
+# -------------
+# to find out a type of variable we can type(<variable_name>)
+# a = int("15")
+# print(a)
+# 10  => "10"
+
+# condition stmt
+# age = input('Enter your age ')
+# age = int(age)
+# if (age > 18 and age <= 100):
+#     print("Eligible")
+# elif (age > 100):
+#     print("Invalid age")
+# else:
+#     print("not eligible")
+
+# loops
+# while
+#     do while
+# for
+
+# a = [1, 2, 3, 4, 5]
+# for _ in range(10):  # [0,1,2,3,4,5,6,7,9]
+#     print(2)
 
